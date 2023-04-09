38eb5c6053448be6bde6cf5907fdd02df1cb8378
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-04-09 17:16:47 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (5.81%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,9 +4,27 @@ public:
         vector<vector<int>>ans(matrix.size(),vector<int>(matrix[0].size()));
         int n=matrix.size();
         int m=matrix[0].size();
+   
+//     1st approach        
+        
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         ans[j][i]=matrix[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<m;j++){
+        //         matrix[i][j]=ans[i][j];
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     reverse(matrix[i].begin(),matrix[i].end());
+        // }
+        
+//     2nd Approach
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                ans[j][i]=matrix[i][j];
+                ans[j][n-1-i]=matrix[i][j];
             }
         }
         for(int i=0;i<n;i++){
@@ -14,8 +32,7 @@ public:
                 matrix[i][j]=ans[i][j];
             }
         }
-        for(int i=0;i<n;i++){
-            reverse(matrix[i].begin(),matrix[i].end());
-        }
+        
+        
     }
 };
