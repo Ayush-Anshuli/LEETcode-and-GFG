9b709cd471cad91ef5b3e4eb0768ce7d74b53825
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-04-12 20:32:37 +0530

    Time: 8 ms (46.38%), Space: 10.5 MB (52.83%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        stack<string> s;
+        string ans="";
+        for(int i=0;i<path.size();i++){
+            string temp="";
+            if(path[i]=='/') continue;            
+                while(path[i]!='/' and i<path.size()){
+                    temp.push_back(path[i++]);
+                }
+                if(temp==".") continue;
+                else if(temp==".."){
+                    if(!s.empty()) {
+                        s.pop();
+                    }
+                }
+            else s.push(temp);
+            }
+        while(!s.empty()){
+            ans="/"+s.top()+ans;
+            s.pop();
+        }
+        if(ans.size()==0){
+            return "/";
+        }
+        return ans;
+        }
+    };
