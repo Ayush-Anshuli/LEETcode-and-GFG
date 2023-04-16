100a2c708e4d493e4396995c3b7d55f600013d54
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-04-16 12:55:41 +0530

    Time: 0 ms (100.00%), Space: 6 MB (52.08%) - LeetHub

---
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool squareIsWhite(string coordinates) {
+        map<char,int>m={{'a',1},{'b',2},{'c',3},{'d',4},{'e',5},{'f',6},{'g',7},{'h',8}};
+        int a1=m[coordinates[0]];
+        int a2=coordinates[1]-'0';
+        
+        if((a1+a2)%2==0) return false;
+        else
+            return true;
+    }
+};
