ce127fe81e6c09a76664283859d5b6f37521cd3f
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-04-27 12:42:55 +0530

    Time: 4 ms (27.68%), Space: 6.3 MB (23.25%) - LeetHub

---
+++ 1507-reformat-date/1507-reformat-date.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string reformatDate(string date) {
+        map<string,string>mp={{"Jan","01"},{"Feb","02"},{"Mar","03"},{"Apr","04"},{"May","05"},{"Jun","06"},{"Jul","07"},{"Aug","08"},{"Sep","09"},{"Oct","10"},{"Nov","11"},{"Dec","12"}};
+        string m="";
+        for(int i=date.size()-8;i<date.size()-5;i++){
+            m+=date[i];
+        }
+        
+        
+        string y="";
+        for(int i=date.size()-4;i<date.size();i++){
+            y+=date[i];
+        }
+        
+        string d="";
+        if(isdigit(date[1])){
+            d+=date[0];
+            d+=date[1];
+        }
+        else{
+            d+="0";
+            d+=date[0];
+        }
+        
+        return y+"-"+mp[m]+"-"+d;
+    }
+};
