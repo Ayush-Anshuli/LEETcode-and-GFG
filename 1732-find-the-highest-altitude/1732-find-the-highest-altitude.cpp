6a2139db0e433cf7c26cc2ca3b8ca9934db5d8a7
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-04-29 16:04:00 +0530

    Time: 3 ms (57.53%), Space: 7.9 MB (75.15%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int>ans;
+        ans.push_back(0);
+        for(int i=0;i<gain.size();i++){
+            ans.push_back(ans.back()+gain[i]);
+        }
+        return *max_element(ans.begin(),ans.end());
+    }
+};
