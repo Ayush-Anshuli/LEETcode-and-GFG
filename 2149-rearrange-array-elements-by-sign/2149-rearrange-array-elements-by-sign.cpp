52d0dc9d0539e3e610788b718aac39092716ba4a
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2024-08-25 17:04:02 +0530

    Time: 135 ms (78.15%), Space: 126.6 MB (73.96%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int>temp1;
-        vector<int>temp2;
-        vector<int>ans;
+//         IT TAKES EXTRA SPACE 
+//         vector<int>temp1;
+//         vector<int>temp2;
+//         vector<int>ans;
         
-        for(int i = 0;i<nums.size();i++){
-            if(nums[i]>0){
-                temp1.push_back(nums[i]);
+//         for(int i = 0;i<nums.size();i++){
+//             if(nums[i]>0){
+//                 temp1.push_back(nums[i]);
+//             }
+//             else temp2.push_back(nums[i]);
+//         }
+//         int i = 0;
+//         int j = 0;
+//         while(i<temp1.size() and j<temp2.size()){
+//             ans.push_back(temp1[i]);
+//             ans.push_back(temp2[j]);
+//             i++;
+//             j++;
+//         }
+//         return ans;
+
+        
+//         TWO POINTER
+        int positive  = 0;
+        int negative = 1;
+        vector<int> ans(nums.size(),0);
+        
+        for(int i =0;i<nums.size();i++){
+            if(nums[i] > 0){
+             ans[positive] = nums[i];
+                positive+=2;
             }
-            else temp2.push_back(nums[i]);
-        }
-        int i = 0;
-        int j = 0;
-        while(i<temp1.size() and j<temp2.size()){
-            ans.push_back(temp1[i]);
-            ans.push_back(temp2[j]);
-            i++;
-            j++;
+            else{
+                ans[negative] = nums[i];
+                negative+=2;
+            }
         }
         return ans;
     }
