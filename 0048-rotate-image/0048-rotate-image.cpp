2e01c871405e96fcdb8b7217f0c4d40fe4eb3606
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2024-08-26 21:20:13 +0530

    Time: 2 ms (47.84%), Space: 8.9 MB (45.54%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,38 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        vector<vector<int>>ans(matrix.size(),vector<int>(matrix[0].size()));
-        int n=matrix.size();
-        int m=matrix[0].size();
-   
-//     1st approach        
-        
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         ans[j][i]=matrix[i][j];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<m;j++){
-        //         matrix[i][j]=ans[i][j];
-        //     }
-        // }
-        // for(int i=0;i<n;i++){
-        //     reverse(matrix[i].begin(),matrix[i].end());
-        // }
-        
-//     2nd Approach
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                ans[j][n-1-i]=matrix[i][j];
+//         OPTIMAL APROACH
+        for(int i=0;i<matrix.size();i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
             }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                matrix[i][j]=ans[i][j];
-            }
+        for(int i = 0; i<matrix.size();i++){
+            reverse(matrix[i].begin(),matrix[i].end());
         }
-        
-        
     }
 };
